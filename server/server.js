f31f1f3773c31cca3a96b9fcb2e86e68e0d56d5f
Evgeniy Bal <evgeniybal@hotmail.com>
2018-03-11 14:23:06 +0200

    Config changed to route angular application

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const distDir = __dirname + '/dist/';
 const app = express();
 app.use(bodyParser.json());
+app.use(express.static(distDir));
 app.use(require('./controllers'));
 
 var {
