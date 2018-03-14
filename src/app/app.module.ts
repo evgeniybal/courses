053c61e82442a6f34628c1d8334167d17ce6d8ca
Evgeniy Bal <evgeniybal@hotmail.com>
2018-03-14 12:31:27 +0200

    Added HttpClientModule to app!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-
+import { HttpClientModule }    from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
@@ -24,11 +23,13 @@ import { AppRoutingModule } from './/app-routing.module';
     BrowserModule,
     FormsModule,
     HttpModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
     UserService,
-    MessageService
+    MessageService   
+
   ],
   bootstrap: [AppComponent]
 })
